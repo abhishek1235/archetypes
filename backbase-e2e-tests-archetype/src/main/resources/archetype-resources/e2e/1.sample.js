3850420a0aeaa3d455cb3a48ad70f763996877bf
aleksandra <aleksandra@backbase.com>
2017-07-11 16:35:40 +0200

    Upgraded package.json: protractor version, added allure reports pom.xml: front-end-maven-plugin version to 1.4 and allure maven plugins protractor.conf: multicapabilities, parameters sample: allure reporting

--- backbase-e2e-tests-archetype/src/main/resources/archetype-resources/e2e/1.sample.js
+++ backbase-e2e-tests-archetype/src/main/resources/archetype-resources/e2e/1.sample.js
@@ -1,25 +1,32 @@
 var SamplePage = require ('../Pages/Sample.js');
 var sample = new SamplePage();
 
-describe ('Sample e2e test', function(){
+describe ('Sample e2e test'+browser.screenType, function(){
 
 	beforeEach(function() { //Necessary if the application does not use Angular or uses timers (or anything that prevents synchronization)
 
-		browser.ignoreSynchronization = true;
-		browser.sleep(2000);
+		browser.ignoreSynchronization = true; //not needed for an angular app
+		browser.manage().timeouts().implicitlyWait(10000);
+        browser.driver.manage().window().maximize();
+        browser.get(testParams.serverUrl);
+        allure.feature('Epic name');//If we use the same feature name in several describes they will be grouped in Allure
 	});
 
+	afterEach(function() {
+        browser.driver.manage().deleteAllCookies(); //This way we can log in again
+    });
 
-	it ('Should have the correct header', function(){
+	it ('Should have the correct header'+browser.screenType, function(){
 
+        allure.story('User story name ');
 		sample.get();
 		expect(sample.header.getText()).toEqual("Example Domain");
 	});
 
 	it ('Should go to iana.org for more info', function(){
 
+        allure.story('User story name ');
 		sample.moreInfo();
-
 		browser.sleep(500);
 		expect(browser.getCurrentUrl()).toBe("http://www.iana.org/domains/reserved");
 	});
--- backbase-e2e-tests-archetype/src/main/resources/archetype-resources/protractor.conf.js
+++ backbase-e2e-tests-archetype/src/main/resources/archetype-resources/protractor.conf.js
@@ -1,39 +1,140 @@
 var JasmineReporters = require('jasmine-reporters');
 var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+var AllureReporter = require('jasmine-allure-reporter');
+var JasmineSpecReporter = require('jasmine-spec-reporter'); //to have verbose report on the console
+
 
 exports.config = {
 	framework: 'jasmine',
 	seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['e2e/1.sample.js'],
 
-	specs: ['e2e/1.sample.js'],
+	multiCapabilities: [
+            {
+                browserName: 'firefox',
+               // specs: ['./tests/smokeTests.js' ] //this way we can select specific tests for an specific capability
+            },
 
-	capabilities: {
-		'browserName': 'chrome'
-	},
+            {
+                browserName: 'chrome'
+            },
+
+            {
+                browserName: 'chrome',
+                chromeOptions: {
+                    args: ['--lang=en',
+                        '--window-size=500,500']
+                }
+            },
+
+            {
+                browserName: 'chrome',
+                chromeOptions: {
+                    mobileEmulation: {
+                        deviceName: 'Nexus 5'
+                    }
+                },
+                //specs: ['./tests/mobile.js' ]
+            },
+
+            {
+                browserName: 'chrome',
+                chromeOptions: {
+                    mobileEmulation: {
+                        deviceName: 'iPad Mini'
+                    }
+                },
+               // specs: ['./tests/overview.js' ]
+            }
+    ],
+
+    params:{//Here we can add all the necessary parameters coming from the command line
+            environment: "", //Default value
+    },
 
 	onPrepare: function() {
-		return browser.getCapabilities().then(function(caps){
-			var browserName = caps.get('browserName').toUpperCase();
-			var browserVersion = caps.get('version');
-			var prefix = browserName + '-' + browserVersion + '-';
-			
-			var junitReporter = new JasmineReporters.JUnitXmlReporter({
-				savePath: 'target/reports/junit',
-				filePrefix: prefix,
-				consolidateAll:false});
 
-			var htmlReporter = new HtmlScreenshotReporter({
-				dest: 'target/reports/html',
-				filename: 'report.html',
-				reportTitle: "E2E test report",
-				pathBuilder: function(currentSpec, suites, browserCapabilities) {
-					// will return chrome/your-spec-name.png
-					return browserCapabilities.get('browserName') + '/' + currentSpec.id;
-				}
-			})
-			
-			jasmine.getEnv().addReporter(junitReporter);
-			jasmine.getEnv().addReporter(htmlReporter);
-		});
-	}
+            return browser.getCapabilities().then(function(caps){
+                browser.browserName = caps.get('browserName').toUpperCase();
+                browser.browserVersion = caps.get('version');
+                browser.prefix = browser.browserName + '-' + browser.browserVersion + '-';
+                browser.manage().timeouts().implicitlyWait(15000);
+                //Broser Type and ScreenType
+                //browser.deviceType -> will be used if we need to define different behaviours between mobile and desktop
+                //browser.screenType -> will be used to print the size of screen or if it is mobile or not
+                browser.getProcessedConfig().then(function (config){
+                    if ("undefined" === typeof config.capabilities.chromeOptions) {
+                        browser.screenType = caps.get('browserName').toUpperCase()+' desktop';
+                        browser.deviceType = 'DESKTOP';
+                    }else if ("undefined" === typeof config.capabilities.chromeOptions.mobileEmulation) {
+                        browser.screenType = caps.get('browserName').toUpperCase()+' desktop '+config.capabilities.chromeOptions.args[1];
+                        browser.deviceType = 'DESKTOP';
+                    } else {
+                        browser.screenType = caps.get('browserName').toUpperCase()+' '+config.capabilities.chromeOptions.mobileEmulation.deviceName.toString();
+                        browser.deviceType = 'MOBILE';
+                    }
+                });
+                switch (browser.params.environment) {
+                    case "LOCAL":
+                        console.log("Local deployment Selected");
+                        testParams = require('./configuration/testdata/local.js');
+                        break;
+                    case "EDIT":
+                        console.log("Edit deployment Selected");
+                        testParams = require('./configuration/testdata/edit.js');
+                        break;
+                    case "STAGING":
+                        console.log("Staging deployment Selected");
+                        testParams = require('./configuration/testdata/staging.js');
+                    case "TEST":
+                        console.log("Test deployment Selected");
+                    default:
+                        console.log("default Selected");
+                        testParams = require('./configuration/testdata/test.js');
+                }
+                browser.baseUrl = testParams.serverUrl;
+
+                var junitReporter = new JasmineReporters.JUnitXmlReporter({
+                    savePath: 'target/reports/junit',
+                    consolidateAll:false});
+
+                var htmlReporter = new HtmlScreenshotReporter({
+                    dest: 'target/reports/html',
+                    filename: 'report.html',
+                    reportTitle: "E2E test report",
+                    pathBuilder: function(currentSpec, suites, browserCapabilities) {
+                        // will return chrome/your-spec-name.png
+                        return browserCapabilities.get('browserName') + '/' + currentSpec.id;
+                    }
+                });
+
+                var allureReporter = new AllureReporter({
+                    resultsDir: 'target/allure-results'
+                });
+
+                var jasmineReporter = new JasmineSpecReporter({
+                       // displayStacktrace: 'all'
+                });
+
+                jasmine.getEnv().afterEach(function(done){
+
+                    browser.takeScreenshot().then(function(png){
+                        allure.createAttachment('Screenshot', function(){
+                            return new Buffer(png, 'base64')
+                        }, 'image/png')();
+                        done();
+                    })
+
+                });
+
+
+                jasmine.getEnv().addReporter(junitReporter);
+                jasmine.getEnv().addReporter(htmlReporter);
+                jasmine.getEnv().addReporter(allureReporter);
+                jasmine.getEnv().addReporter(jasmineReporter);
+
+            });
+            console.log("End of on prepare!");
+    }
+
 }
